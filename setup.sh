b5f8dc2b4f7d9e6d9b4767671f340d10ca48b11d
JohnnySn0w <33181572+JohnnySn0w@users.noreply.github.com>
2024-03-03 17:14:18 -0600

    Add defaultModels.sh
    - Update README.md
    - Rename runAMD.sh and buildAMD.sh to remove AMD
    - Update run.sh

--- setup.sh
+++ setup.sh
@@ -1,10 +1,40 @@
 #!/bin/bash
-pipx install piper-tts 
-if [[ ! -d llms ]]; then
-  mkdir llms
-fi
-if [[ ! -d voices ]]; then
-  mkdir voices;
-fi
-echo "Directories ready for downloads";
-./buildAMD.sh
+# run in root of git repo
+
+
+# Define directories to ensure they exist
+directories=("llms" "voices")
+
+# Create directories if they don't exist
+for dir in "${directories[@]}"; do
+  if [[ ! -d "$dir" ]]; then
+    echo "Creating directory: $dir"
+    mkdir "$dir"
+  fi
+done
+
+echo "Directories ready for downloads."
+
+# Initialize and update git submodules
+echo "Initializing git submodules..."
+git submodule init
+git submodule update
+echo "Pulled submodules, building..."
+
+# Run build script
+./build.sh
+
+# Setup Python virtual environment and install dependencies
+echo "Setting up Python virtual environment..."
+python3 -m venv venv
+source ./venv/bin/activate
+
+# Install Python dependencies
+echo "Installing Python dependencies..."
+cd piper/src/python_run || exit
+pip install -r requirements.txt
+pip install -r requirements_http.txt
+
+
+
+echo "Setup complete."
