83c089b13d43d8b8ad7e279e55c2f9c5e78e9e71
JohnnySn0w <33181572+JohnnySn0w@users.noreply.github.com>
2024-08-31 17:42:26 -0500

    Add easy process kill

--- run.sh
+++ run.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
-nohup ./whisper.cpp/server -m ./whisper.cpp/models/ggml-large-v3-q5_0.bin --port 6666 --no-timestamps > ./whisper.cpp/whisper.log &
-nohup ./llama.cpp/server -m ./llms/capybarahermes-2.5-mistral-7b.Q4_K_M.gguf -ngl 1000 --port 7777 > ./llama.cpp/llama.log &
+nohup ./whisper.cpp/server -m ./whisper.cpp/models/ggml-large-v3-q5_0.bin --port 6666 --no-timestamps > ./whisper.cpp/whisper.log & echo $! > run.pid
+nohup ./llama.cpp/server -m ./llms/capybarahermes-2.5-mistral-7b.Q4_K_M.gguf -ngl 1000 --port 7777 > ./llama.cpp/llama.log & echo $! >> run.pid
 source ./venv/bin/activate
 cd piper/src/python_run
-nohup python -m piper.http_server -m ../../../voices/en_US-kusal-medium.onnx > piper.log &
+nohup python -m piper.http_server -m ../../../voices/en_GB-alba-medium.onnx > piper.log & echo $! >> ../../../run.pid
